d3e7cee2e02ad4d3a815a98d248d866643137024
Justin Gray <justin.s.gray@gmail.com>
2025-06-13 19:24:26 -0400

    adding property definitions

--- post_code.js
+++ post_code.js
@@ -1,3 +1,43 @@
+properties = {
+  // <your other post properties>
+  // .
+  // .
+  wcsTolerance: 0.01, // tolerance for checking WCS offsets
+  wcsXShift: 0.0, // WCS X Shift for macro validation
+  wcsYShift: 0.0, // WCS Y Shift for macro validation
+  wcsZShift: 0.0, // WCS Z Shift for macro validation
+}
+
+propertyDefinitions = {
+  // <your other post property descriptions>
+  // .
+  // .
+  wcsTolerance: {
+    title: "WCS tolerance",
+    description: "Enter the tolerance for checking WCS origins",
+    group: 1,
+    type: "spatial",
+  },
+  wcsXShift: {
+    title: "WCS X Offset",
+    description: "WCS Shift for X to align machine model with real life",
+    group: 1,
+    type: "spatial",
+  },
+  wcsYShift: {
+    title: "WCS Y Offset",
+    description: "WCS Shift for Y to align machine model with real life",
+    group: 1,
+    type: "spatial",
+  },
+  wcsZShift: {
+    title: "WCS Z Offset",
+    description: "WCS Shift for Z to align machine model with real life",
+    group: 1,
+    type: "spatial",
+  },
+}
+
 function toMCSPosition(section, wcsPosition) {
   var partAttachPoint = section.getPartAttachPoint();
   var tableAttachPoint = machineConfiguration.getTableAttachPoint();
